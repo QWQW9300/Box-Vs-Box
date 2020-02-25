23a77bcbf392f7c5497b73e2ed7f8b54fb182ee3
qwqw9300 <360804671@qq.com>
2020-02-25 21:03:29 +0800

    20200225 Construct

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1582635649,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/platform-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/tiledbackground.png",
+		"images/particles.png",
+		"images/enemy_0-sheet0.png",
+		"images/gauge-sheet0.png",
+		"images/gauge-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/sprite2-sheet2.png",
+		"images/enemy_bullet2-sheet0.png",
+		"images/particles2.png",
+		"images/cross.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
